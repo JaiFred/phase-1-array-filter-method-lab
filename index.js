8811166585e4e9c9dad727ed2a9e75a606dfc833
JaiVon <frederickjaivon@yahoo.com>
2022-06-23 22:24:15 -0400

    commit tree 105a3f2a0fa6cf9764a2f2e0e6e68690e996b258

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// function findMatching(drivers, string){
+//     let response = drivers.filter(element => element === string || element === string.toLowerCase)
+//     return response
+// }
+
+// function fuzzyMatch(drivers, string){
+//     let firstLetter = drivers.filter(driver => driver.slice(0, string.length) === string)
+//     return firstLetter
+// }
+
+function findMatching(array, string) {
+    return array.filter(function(el) {
+        return el.toLowerCase().indexOf(string.toLowerCase())!== -1
+    })
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(function(el) {
+        return el.startsWith(string)
+    })
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(driver){
+      return driver.name === name
+    })
+  }
